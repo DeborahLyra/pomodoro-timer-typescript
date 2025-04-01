d1c24fb1b6abe50c4f6e5fb152b0cdfc1336363b
Deborah Lyra <deborahp.lyra@gmail.com>
2025-04-01 11:31:00 -0300

    edited: header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,16 @@
+import { Scroll, Sun, Timer } from "@phosphor-icons/react";
+import { HeaderContainer } from "./style";
+import { NavLink } from "react-router-dom";
 
 
 export function Header() {
   return (
-    <div>
-      <p>Header</p>
-    </div>
+    <HeaderContainer>
+      <Sun size={24}/>
+      <nav>
+        <NavLink to="/"><Timer size={24}/></NavLink>
+        <NavLink to="/history"><Scroll size={24}/></NavLink>
+      </nav>
+    </HeaderContainer>
   )
 }
